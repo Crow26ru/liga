a24987c18b112ddebf65bdfa815f16568f83e23f
Odlopez <Odlopez@inbox.ru>
2019-10-23 17:05:00 +0300

    Исправляет замечания по слайдерам

--- src/js/projects-slider.js
+++ src/js/projects-slider.js
@@ -3,6 +3,7 @@
 (function() {
   var swiper = new Swiper('.projects__slider', {
     slidesPerView: 1,
+    loop: true,
     navigation: {
       nextEl: '.projects__button--next',
       prevEl: '.projects__button--prev',
